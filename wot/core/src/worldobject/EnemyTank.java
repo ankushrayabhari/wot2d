37dae15c6da7c1ab9fea9fd1503fb7e3ee5a5202
andrewsi10 <andrewsi10@gmail.com>
2016-12-11 21:09:57 -0500

    added enemy AI movement capability

--- wot/core/src/worldobject/EnemyTank.java
+++ wot/core/src/worldobject/EnemyTank.java
@@ -44,6 +44,7 @@ public class EnemyTank extends TankObject {
     private OrthographicCamera camera;
     private World world;
     private RayCaster rayCaster;
+    private boolean chase = false;
     
     public EnemyTank(World world, OrthographicCamera camera, GameMap map, 
             Sprite[] sprites, float pixels_per_meter,
@@ -56,10 +57,66 @@ public class EnemyTank extends TankObject {
         setUserData(getHull(), getTurret());
         rayCaster = new RayCaster();
     }
+    
+    public boolean clearPath() {
+        world.rayCast(rayCaster, getHull().getPosition(), 
+                player.getHull().getPosition());
+        rayCaster.setMinFraction(10000);
+        if (rayCaster.getFirstFixture() != null) {
+            return (rayCaster.getFirstFixture().getFilterData().categoryBits == 0x0001);
+        }
+        return false;
+    }
+    
+    public float getDistance() {
+        return (float) (Vector2.dst2(getHull().getPosition().x, 
+                getHull().getPosition().y, 
+                player.getHull().getPosition().x, player.getHull().getPosition().y));
+    }
 
     @Override
     public float getHullTorque() {
-        // TODO Auto-generated method stub
+        if (GameMap.withinRenderRange(camera, getX(), getY()) && clearPath()) {
+            chase = true;
+        }
+        float finalAngle = (float) ((Math.atan2(player.getY() - getY(), player.getX() - 
+                getX()) + 2 * Math.PI) % (2 * Math.PI));
+        float angle = getHull().getAngle();
+        while (angle < 0) {
+            angle += Math.PI * 2;
+        }
+        if (angle >= Math.PI * 2) {
+            angle %= Math.PI * 2;
+        }
+        while (finalAngle < 0) {
+            finalAngle += Math.PI * 2;
+        }
+        if (finalAngle >= Math.PI * 2) {
+            finalAngle %= Math.PI * 2;
+        }
+        double deltaThreshold = Math.PI / 4;
+        if (chase) {
+            deltaThreshold = 0;
+        }
+        if (Math.abs(finalAngle - angle) > deltaThreshold && 
+                (GameMap.withinRenderRange(camera, getX(), getY()) || clearPath())) {
+            if (angle >= Math.PI && angle <= Math.PI * 2) {
+                if (finalAngle >= angle || finalAngle <= angle - Math.PI) {
+                    return 1;
+                }
+                return -1;
+            } else if (angle >= Math.PI / 2 && angle <= Math.PI) {
+                if (finalAngle >= angle && finalAngle <= angle + Math.PI) {
+                    return 1;
+                }
+                return -1;
+            } else {
+                if (finalAngle <= angle + Math.PI && finalAngle >= angle) {
+                    return 1;
+                }
+                return -1;
+            }
+        }
         return 0;
     }
 
@@ -70,32 +127,82 @@ public class EnemyTank extends TankObject {
 
     @Override
     public float getHullForceX() {
-        // TODO Auto-generated method stub
-        return 0;
+        float forceX = 0;
+        float finalAngle = (float) ((Math.atan2(player.getY() - getY(), player.getX() - 
+                getX()) + 2 * Math.PI) % (2 * Math.PI));
+        float angle = getHull().getAngle();
+        while (angle < 0) {
+            angle += Math.PI * 2;
+        }
+        if (angle >= Math.PI * 2) {
+            angle %= Math.PI * 2;
+        }
+        while (finalAngle < 0) {
+            finalAngle += Math.PI * 2;
+        }
+        if (finalAngle >= Math.PI * 2) {
+            finalAngle %= Math.PI * 2;
+        }
+        if (chase == true) {
+            if (Math.abs(finalAngle - angle) >= Math.PI / 2 || getDistance() < 20) {
+                forceX = (float)(-forceMultiplier * 1.5f / 2 *
+                        Math.cos(getHull().getAngle()));
+            } else if (getDistance() > 40) {
+                forceX = (float)(forceMultiplier * 
+                        Math.cos(getHull().getAngle()));
+            }
+        }
+        return forceX;
     }
 
     @Override
     public float getHullForceY() {
-        // TODO Auto-generated method stub
-        return 0;
+        float forceY = 0;
+        float finalAngle = (float) ((Math.atan2(player.getY() - getY(), player.getX() - 
+                getX()) + 2 * Math.PI) % (2 * Math.PI));
+        float angle = getHull().getAngle();
+        while (angle < 0) {
+            angle += Math.PI * 2;
+        }
+        if (angle >= Math.PI * 2) {
+            angle %= Math.PI * 2;
+        }
+        while (finalAngle < 0) {
+            finalAngle += Math.PI * 2;
+        }
+        if (finalAngle >= Math.PI * 2) {
+            finalAngle %= Math.PI * 2;
+        }
+        if (chase == true) {
+            if (Math.abs(finalAngle - angle) >= Math.PI / 2 || getDistance() < 20) {
+                forceY = (float)(-forceMultiplier * 1.5f / 2 *
+                        Math.sin(getHull().getAngle()));
+            } else if (getDistance() > 40) {
+                forceY = (float)(forceMultiplier * 
+                        Math.sin(getHull().getAngle()));
+            }
+        }
+        return forceY;
     }
 
     @Override
     public boolean isShooting() {
-        world.rayCast(rayCaster, getHull().getPosition(), 
-                player.getHull().getPosition());
-        rayCaster.setMinFraction(10000);
-        if (rayCaster.getFirstFixture() != null) {
-            return (GameMap.withinRenderRange(camera, getX(), getY()) && 
-                    (rayCaster.getFirstFixture().getFilterData().categoryBits == 0x0001));
-        }
+//        return (GameMap.withinRenderRange(camera, getX(), getY()) && 
+//                clearPath());
         return false;
     }
 
     @Override
     public float getAimingAngleInRad() {
-        return (float) ((Math.atan2(player.getY() - getY(), player.getX() - 
-                getX()) + 2 * Math.PI) % (2 * Math.PI));
+        double angle = (Math.atan2(player.getY() - getY(), player.getX() - 
+                getX()) + 2 * Math.PI) % (2 * Math.PI);
+        while (angle < 0) {
+            angle += Math.PI * 2;
+        }
+        if (angle >= Math.PI * 2) {
+            angle %= Math.PI * 2;
+        }
+        return (float) angle;
     }
 
 }
